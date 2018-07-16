f0556b1d8ee5d995e796103c37d96af7ab3ab194
Ben Hamilton <benhamilton@google.com>
2018-07-16 16:01:55 -0600

    Initial Commit

---
+++ ChipsFieldInputController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChipsFieldInputController
+//
+//  Created by Ben Hamilton on 7/16/18.
+//  Copyright © 2018 Ben Hamilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChipsFieldInputController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChipsFieldInputController
+//
+//  Created by Ben Hamilton on 7/16/18.
+//  Copyright © 2018 Ben Hamilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
